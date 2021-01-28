2339511928b6bb9f7ab6721c30556d6d859e02e3
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-01-29 00:59:03 +0530

    Java Stdin and Stdout II
    Input Format
    
    There are three lines of input:
    
    The first line contains an integer.
    The second line contains a double.
    The third line contains a String.
    Output Format
    
    There are three lines of output:
    
    On the first line, print String: followed by the unaltered String read from stdin.
    On the second line, print Double: followed by the unaltered double read from stdin.
    On the third line, print Int: followed by the unaltered integer read from stdin.

---
+++ Hacker Rank/Program4.java
@@ -0,0 +1,22 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a;
+		double b;
+		String c;
+		a = scan.nextInt();
+		b = scan.nextDouble();
+		c = scan.nextLine();
+		c=scan.nextLine();
+		System.out.println("String: " + c);
+		System.out.println("Double: " + b);
+		System.out.println("Int: " + a);
+
+	}
+
+}
