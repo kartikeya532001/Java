5f78c00a6ba6a0501b70be789d4ed3f3d320cffe
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-02-02 00:08:47 +0530

    Count Divisors

--- Hacker Earth/Program7.java
+++ Hacker Earth/Program7.java
@@ -3,7 +3,18 @@ package HackerEarth;
 public class Program7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a, b, c, j = 0;
+		a = scan.nextInt();
+		b = scan.nextInt();
+		c = scan.nextInt();
+		for (int i = a; i <= b; i++) {
+			if (i % c == 0) {
+				j++;
+			}
+		}
+		System.out.println(j);
+		scan.close();
 
 	}
 
