5245a8b8be8976669d530ba2e4499d8696922f60
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-03-22 02:48:04 +0530

    Add files via upload

---
+++ Hacker Rank/SimpleArraySum.java
@@ -0,0 +1,19 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class SimpleArraySum {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n, sum = 0;
+		n = scan.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = scan.nextInt();
+			sum = sum + a[i];
+		}
+		System.out.println(sum);
+		scan.close();
+	}
+
+}
