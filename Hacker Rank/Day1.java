f68f3c48bd8a5bf7de525bbd96db9916b63209ca
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-05-01 00:45:12 +0530

    Add files via upload

---
+++ Hacker Rank/Day1.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int i = 4;
+		double d = 4.0;
+		String s = "HackerRank ";
+		int a = scan.nextInt();
+		double e = scan.nextDouble();
+		scan.nextLine();
+		String r = scan.nextLine();
+		System.out.println(a + i);
+		System.out.println(d + e);
+		System.out.println(s + r);
+
+		scan.close();
+	}
+
+}
