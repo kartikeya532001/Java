eae906a8a03b9dc56090654954feeb8b6ee513e4
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-05-01 00:44:49 +0530

    Add files via upload

---
+++ College work/BalanceAcc.java
@@ -0,0 +1,10 @@
+import collegework.Packages_6_2;
+
+public class BalanceAcc {
+	public static void main(String[] args) {
+		Packages_6_2 b1 = new Packages_6_2();
+		double rem = b1.Display_Balance(70000.0, 10000.0);
+		System.out.println(rem);
+	}
+
+}
